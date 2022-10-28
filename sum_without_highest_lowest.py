8db6046abe3c9d403df36a27a0770d70e47377fb
Jay <48065168+Yozora-7@users.noreply.github.com>
2022-10-28 18:32:46 +0100

    Update sum_without_highest_lowest.py

--- sum_without_highest_lowest.py
+++ sum_without_highest_lowest.py
@@ -1,5 +1,11 @@
 # sum without highest and lowest number
 
+# sum all the numbers of a given array (cq.list), except the highest and the lowest element (by value,
+# not by index!)
+
+# the highest or lowest element respectively is a single element at each edge, even if there are more
+# than one with the same value.
+
 def sum_array(arr):
   return 0 if arr == None or len(arr) < 3 else sum(arr) - min(arr) - max(arr))
 
