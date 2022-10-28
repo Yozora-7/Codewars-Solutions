328bbf4f954fc5f8a12477c327731cff2338225a
Jay <48065168+Yozora-7@users.noreply.github.com>
2022-10-28 17:58:30 +0100

    Create convert_num_to_string.py

---
+++ convert_num_to_string.py
@@ -0,0 +1,8 @@
+# convert a number to a string
+
+# we need a function that can transform a number (integer) into a string.
+
+# what ways of achieving this do you know?
+
+def number_to_string(num):
+  return str(num)
