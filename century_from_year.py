42288eac0acd6cee416a61efb28930b21e768526
Jay <48065168+Yozora-7@users.noreply.github.com>
2022-10-28 23:02:59 +0100

    Create century_from_year.py

---
+++ century_from_year.py
@@ -0,0 +1,10 @@
+"""
+Introduction
+The first century spans from the year 1 up to and including the year 100, the second century - from the year 101 up to and including the year 200, etc.
+
+Task
+Given a year, return the century it is in.
+"""
+
+def century(year):
+    return (year - 1) //  100 + 1 # tp calculate the century would be to divide the year by 100 + 1  for odd numbers (floor division)
