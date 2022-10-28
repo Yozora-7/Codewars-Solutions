862749887938c39df4176d93cf187ae4c68e3718
Jay <48065168+Yozora-7@users.noreply.github.com>
2022-10-29 00:29:24 +0100

    Update bmi_calculator.py

--- bmi_calculator.py
+++ bmi_calculator.py
@@ -22,3 +22,8 @@ def bmi(weight, height):
 def bmi(weight, height):
     b = weight / height ** 2
     return ['Underweight', 'Normal', 'Overweight', 'Obese'][(b > 30) + (b > 25) + (b > 18.5)]
+
+"""
+you can add boolean values in Python (True -> 1, False -> 0). For example, if b == 27 then you get for 
+(b > 30) + (b > 25) + (b > 18.5) -> False + True + True -> 0 + 1 + 1 -> 2 ['Underweight', 'Normal', 'Overweight', 'Obese'][2] -> 'Overweight'
+"""
