bab5200f30f10654c11f8a21bc69430a5a2c8bdc
Jay <48065168+Yozora-7@users.noreply.github.com>
2022-10-29 00:13:13 +0100

    Update century_from_year.py

--- century_from_year.py
+++ century_from_year.py
@@ -8,3 +8,10 @@ Given a year, return the century it is in.
 
 def century(year):
     return (year - 1) //  100 + 1 # tp calculate the century would be to divide the year by 100 + 1  for odd numbers (floor division)
+
+# another example
+
+import math
+
+def century(year):
+    return math.ceil(year / 100)
