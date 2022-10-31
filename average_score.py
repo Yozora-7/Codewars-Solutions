52236310056bac2287ecc1b853486c473e6aac2a
Jay <48065168+Yozora-7@users.noreply.github.com>
2022-10-31 11:03:27 +0000

    Create average_score.py

---
+++ average_score.py
@@ -0,0 +1,21 @@
+"""
+DESCRIPTION:
+There was a test in your class and you passed it. Congratulations!
+But you're an ambitious person. You want to know if you're better than the average student in your class.
+
+You receive an array with your peers' test scores. Now calculate the average and compare your score!
+
+Return True if you're better, else False!
+
+Note:
+Your points are not included in the array of your class's points. For calculating the average point you may add your point to the given array!
+"""
+
+def better_than_average(class_points, your_points):
+    overall = sum(class_points) / len(class_points)
+    return False if overall > your_points else True
+  
+# another example
+
+def better_than_average(class_points, your_points):
+    return your_points > sum(class_points) / len(class_points)
