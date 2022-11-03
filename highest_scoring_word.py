03fa296b1ca5394828f557951ba7b572f41a2b04
Jay <48065168+Yozora-7@users.noreply.github.com>
2022-11-03 10:07:02 +0000

    Create highest_scoring_word.py

---
+++ highest_scoring_word.py
@@ -0,0 +1,20 @@
+"""
+DESCRIPTION:
+Given a string of words, you need to find the highest scoring word.
+
+Each letter of a word scores points according to its position in the alphabet: a = 1, b = 2, c = 3 etc.
+
+You need to return the highest scoring word as a string.
+
+If two words score the same, return the word that appears earliest in the original string.
+
+All letters will be lowercase and all inputs will be valid.
+"""
+
+def high(words):
+    return max(words.split(), key=lambda word: sum(map(ord, word)) - 96*len(word))
+  
+# another example
+
+def high(x):
+    return max(x.split(), key=lambda k: sum(ord(c) - 96 for c in k))
