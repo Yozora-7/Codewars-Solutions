bdd60436be8b08c9d268bd4511492277135a2f4b
Jay <48065168+Yozora-7@users.noreply.github.com>
2022-11-04 10:19:49 +0000

    Create build_tower.py

---
+++ build_tower.py
@@ -0,0 +1,35 @@
+"""
+DESCRIPTION:
+Build Tower
+Build a pyramid-shaped tower, as an array/list of strings, given a positive integer number of floors. A tower block is represented with "*" character.
+
+For example, a tower with 3 floors looks like this:
+
+[
+  "  *  ",
+  " *** ", 
+  "*****"
+]
+And a tower with 6 floors looks like this:
+
+[
+  "     *     ", 
+  "    ***    ", 
+  "   *****   ", 
+  "  *******  ", 
+  " ********* ", 
+  "***********"
+]
+
+"""
+
+def tower_builder(n):
+    l = []
+    for i in range(1,n+1):
+        l.append(' '*(n-i)+'*'*(i+(i-1))+' '*(n-i))
+    return l
+  
+# another example
+
+def tower_builder(n):
+    return [("*" * (i*2-1)).center(n*2-1) for i in range(1, n+1)]
