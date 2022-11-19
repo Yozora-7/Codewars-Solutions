e86ee7e95bfbedf11e3d29f12e693e7ace752e3e
Jay <48065168+Yozora-7@users.noreply.github.com>
2022-11-19 23:45:57 +0000

    Create name_shuffler.py

---
+++ 8 Kyu/name_shuffler.py
@@ -0,0 +1,11 @@
+"""
+
+"""
+
+def name_shuffler(s: str):
+    return ' '.join(reversed(list(s.split(' '))))
+  
+# another example
+
+def name_shuffler(str_):
+    return ' '.join(str_.split(' ')[::-1])
