407cb8bb8833873c73d1628e6d9661918c21f2be
Jay <48065168+Yozora-7@users.noreply.github.com>
2022-11-19 23:46:28 +0000

    Update name_shuffler.py

--- 8 Kyu/name_shuffler.py
+++ 8 Kyu/name_shuffler.py
@@ -1,5 +1,10 @@
 """
+DESCRIPTION:
+Write a function that returns a string in which firstname is swapped with last name.
 
+Example(Input --> Output)
+
+"john McClane" --> "McClane john"
 """
 
 def name_shuffler(s: str):
