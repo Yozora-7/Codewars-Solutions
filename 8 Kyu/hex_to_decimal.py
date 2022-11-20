c4d2c712f22c401bb4c78397cc79ea9fd2661008
Jay <48065168+Yozora-7@users.noreply.github.com>
2022-11-20 00:38:42 +0000

    Create hex_to_decimal.py

---
+++ 8 Kyu/hex_to_decimal.py
@@ -0,0 +1,12 @@
+"""
+Complete the function which converts hex number (given as a string) to a decimal number.
+"""
+
+def hex_to_dec(s):
+    s = int(s, base = 16)
+    return s
+  
+# another example
+
+def hex_to_dec(s):
+    return int(s, 16)
